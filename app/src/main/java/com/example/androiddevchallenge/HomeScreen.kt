83c0728908eae444fb0e26852434bd2a983cc933
ochi <mune.ochi@gmail.com>
2021-03-17 23:35:45 +0900

    created the bottom navigation

--- app/src/main/java/com/example/androiddevchallenge/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/HomeScreen.kt
@@ -15,13 +15,84 @@
  */
 package com.example.androiddevchallenge
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
 fun HomeScreen() {
-    Text(text = "Home", modifier = Modifier.padding(24.dp))
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                class Nav(val title: String, val resource: Int)
+
+                val navs = listOf(
+                    Nav("Home", R.drawable.ic_baseline_home_24),
+                    Nav("Favorite", R.drawable.ic_baseline_favorite_border_24),
+                    Nav("Profile", R.drawable.ic_baseline_account_circle_24),
+                    Nav("Cart", R.drawable.ic_baseline_shopping_cart_24),
+                )
+                var selected by remember { mutableStateOf(0) }
+
+                navs.forEachIndexed { index, nav ->
+                    BottomNavigationItem(
+                        selected = selected == index,
+                        label = {
+                            Text(
+                                text = nav.title,
+                                style = MaterialTheme.typography.caption,
+                            )
+                        },
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = nav.resource),
+                                contentDescription = nav.title,
+                            )
+                        },
+                        onClick = {
+                            selected = index
+                        }
+                    )
+                }
+            }
+        }
+    ) {
+        NavHost(navController, startDestination = "bHome") {
+            composable("bHome") { HomeScreen() }
+            composable("Favorite") { }
+            composable("Profile") { }
+            composable("Cart") { }
+        }
+    }
+}
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun HomeLightPreview() {
+    MyTheme {
+        HomeScreen()
+    }
+}
+
+@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun HomeDarkPreview() {
+    MyTheme(darkTheme = true) {
+        HomeScreen()
+    }
 }
